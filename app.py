b34319e1d28e19be282a24f167c52386d4d339c3
Krishna Muhammad <Krishna.Muhammad@gmail.com>
2021-01-26 13:12:06 -0600

    Modified: request branch Request branch Modified : request branch

--- app.py
+++ app.py
@@ -1,1 +1,33 @@
-new repo for twitterteam
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello World!'
+
+@app.route('/about')
+def about():
+
+    about_text = """
+    Fill in some info about the project here.
+    """
+    return about_text
+    
+if __name__=='__main__':
+
+    app.run(debug=True)
+
+@app.route('/')
+def home():
+
+    # if request has args (?tweet=some_tweet)
+    if request.args:
+
+        # get the value of the arg
+        tweet = request.args.get('tweet')   
+        
+        return tweet
+
+    return 'Hello World!'
